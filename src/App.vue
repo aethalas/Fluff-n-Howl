947ad08f7a11c3af2240aba0ffddbf6bb64b710a
Jonny Tyson <jonnytyson@live.com>
2019-08-24 15:25:55 -0700

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+ body {
+    background: #ffe481;
+    margin: 0;
+  }
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="../assets/header.png">
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  img {
+    width: 50%;
+  }
+
+ @media (max-width: 1400px) {
+   img {
+     width: 100%;
+   }
+ }
+</style>
